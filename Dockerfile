f6e322037b79e24947f7d0a58f22d44a46b68b04
Hristijan Ivanovski <hristijan.ivanovski1993@yahoo.com>
2023-03-31 17:10:30 +0200

    m

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@ ENV _AIRFLOW_WWW_USER_CREATE_EMAIL=admin@example.com
 ENV _AIRFLOW_WWW_USER_CREATE_FIRSTNAME=Airflow
 ENV _AIRFLOW_WWW_USER_CREATE_LASTNAME=Admin
 
-# Copy your DAG script to the DAGs folder in the container
+# Copy the DAG script to the DAGs folder in the container
 COPY airflow_dag_task.py /opt/airflow/dags/
 
-# Copy custom Airflow configuration
+# Copy the Airflow configuration
 COPY airflow.cfg /opt/airflow/airflow.cfg
 
-# Add entrypoint script and set permissions
+# Adding entrypoint script and setting permissions
 COPY entrypoint.sh /entrypoint.sh
 USER root
 RUN chmod +x /entrypoint.sh && chown airflow: /entrypoint.sh
--- airflow_dag_task.py
+++ airflow_dag_task.py
@@ -2,6 +2,7 @@ import warnings
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+import psycopg2
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -10,6 +11,28 @@ def print_time_only(**kwargs):
     now = datetime.now()
     formatted_time = now.strftime('%Y.%m.%d %H:%M:%S')
     print(f'Current Time: {formatted_time}')
+
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(
+        host="localhost",
+        database="afdagtasktimedb",
+        user="postgres",
+        password="postgres"
+    )
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Insert data into the table
+    cur.execute("INSERT INTO afdagtasktimedb (time) VALUES (%s)",
+                (formatted_time,))
+
+    # Commit changes
+    conn.commit()
+
+    # Close cursor and connection
+    cur.close()
+    conn.close()
 
 
 default_args = {
